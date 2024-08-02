2d1b2d91c40163b6055ad109aa0626d37432226e
Enak Junior <mamadoukanejunior@gmail.com>
2024-08-02 12:41:22 +0000

    projet ProjectManagerFront first commit...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: [
+    './app.component.css',
+    '/node_modules/bootstrap/dist/css/bootstrap.min.css',
+    '/node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+  ]
 })
 export class AppComponent {
   title = 'ProjectManagerFront';
